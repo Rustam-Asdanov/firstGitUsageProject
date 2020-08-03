ce6c4d1535121106c277ecdadebfdc8792492e34
Krosent <krosent2@gmail.com>
2020-08-03 20:53:04 +0400

    This is our second commit, we added new function!

--- src/com/company/rustam/Main.java
+++ src/com/company/rustam/Main.java
@@ -3,7 +3,14 @@ package com.company.rustam;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hello World!");
+
+        getPerson("Rustam");
     }
+
+
+    public static void getPerson(String name) {
+        System.out.println("Hello >> " + name);
+    }
+
 }
